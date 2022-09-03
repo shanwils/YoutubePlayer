45bdc041a504aaad137ca81f0ff80460efe1b526
shanwils <64047685+shanwils@users.noreply.github.com>
2022-09-03 16:00:05 -0400

    Initial Commit

---
+++ app/src/main/java/shannonwilson/example/org/MainActivity.java
@@ -0,0 +1,44 @@
+package shannonwilson.example.org;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSingle = (Button) findViewById(R.id.btnPlaySingle);
+        Button btnStandalone = (Button) findViewById(R.id.btnStandalone);
+
+        btnSingle.setOnClickListener(this);
+        btnStandalone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+
+        switch(view.getId()){
+            case R.id.btnPlaySingle:
+                intent = new Intent(this, YoutubeActivity.class);
+                break;
+
+            case R.id.btnStandalone:
+                intent = new Intent(this, StandAloneActivity.class);
+                break;
+
+            default:
+        }
+
+        if(intent != null){
+            startActivity(intent);
+        }
+    }
+}
